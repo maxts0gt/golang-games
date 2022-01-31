ec8c15a8bad74af904dc8ea19d87ef9fe70c5778
Max <bazarsadtsogtjargal@gmail.com>
2022-01-31 17:27:44 +0900

    handle game over

--- hangman/main.go
+++ hangman/main.go
@@ -30,7 +30,8 @@ func main() {
 	randomWord := getRandomWord()
 	guessedLetters := initializeGuessedLetters(randomWord)
 	hangmanState := 0
-	for {
+
+	for !isGameOver(randomWord, guessedLetters, hangmanState) {
 		printGameState(randomWord, guessedLetters, hangmanState)
 		input := readInput()
 		if len(input) != 1 {
@@ -45,6 +46,13 @@ func main() {
 		}
 	}
 
+	fmt.Print("Game Over. ")
+	if isWordGuessed(randomWord, guessedLetters) {
+		fmt.Println("You win!")
+	} else if isHangmanComplete(hangmanState) {
+		fmt.Println("You lose!")
+	}
+
 }
 
 func getRandomWord() string {
@@ -57,6 +65,24 @@ func initializeGuessedLetters(randomWord string) map[rune]bool {
 	guessedLetters[unicode.ToLower(rune(randomWord[0]))] = true
 	guessedLetters[unicode.ToLower(rune(randomWord[len(randomWord)-1]))] = true
 	return guessedLetters
+}
+
+func isGameOver(randomWord string, guessedLetters map[rune]bool, hangmanState int) bool {
+	return isWordGuessed(randomWord, guessedLetters) || isHangmanComplete(hangmanState)
+
+}
+
+func isWordGuessed(randomWord string, guessedLetters map[rune]bool) bool {
+	for _, ch := range randomWord {
+		if !guessedLetters[unicode.ToLower(ch)] {
+			return false
+		}
+	}
+	return true
+}
+
+func isHangmanComplete(hangmanState int) bool {
+	return hangmanState >= 9
 }
 
 func printGameState(randomWord string, guessedLetters map[rune]bool, hangmanState int) {
